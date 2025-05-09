9c15fdeedccdd7f4459896e4679458f490adaf8e
SargisSuqiasyan <97920258+Basturman@users.noreply.github.com>
2025-05-10 01:46:53 +0400

    first

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'gear-app-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './sw.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
